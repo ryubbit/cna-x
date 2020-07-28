4ea0c710ec1b423f4624de898c7b7430ddebca69
ryubbit <SKCC@SKCC16D00064.SKCC.NET>
2020-07-28 15:26:19 +0900

    1st commit

---
+++ order/src/main/java/demo/Order.java
@@ -0,0 +1,69 @@
+package demo;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Order_table")
+public class Order {
+
+    private Long id;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer productId;
+    private Integer qty;
+
+    @PostPersist
+    public void onPostPersist(){
+        Ordered ordered = new Ordered();
+        BeanUtils.copyProperties(this, ordered);
+        ordered.publishAfterCommit();
+
+
+    }
+
+    @PreRemove
+    public void onPreRemove(){
+        OrderCanceled orderCanceled = new OrderCanceled();
+        BeanUtils.copyProperties(this, orderCanceled);
+        orderCanceled.publishAfterCommit();
+
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+
+        demo.external.Cancellation cancellation = new demo.external.Cancellation();
+        // mappings goes here
+        OrderApplication.applicationContext.getBean(demo.external.CancellationService.class)
+            .cancel(cancellation);
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+
+
+
+}
